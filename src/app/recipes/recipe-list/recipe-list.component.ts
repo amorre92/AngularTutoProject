daa70ffce52c5025354eaf4fbf1b2132bacaf80b
Anthony Morre <amorre92@gmail.com>
2019-12-10 14:28:44 +0100

    lecture 52

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'yummy test', 'https://www.google.be/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwje5IvulavmAhXoDmMBHZcMBVMQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.gimmesomeoven.com%2Fjambalaya-recipe%2F&psig=AOvVaw02p8qz5AUbH1EZFe_jzLlt&ust=1576070386248888')
+  ];
 
   constructor() { }
 
