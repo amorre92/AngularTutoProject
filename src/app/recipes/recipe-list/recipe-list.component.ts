238a69923efa70d478881a3e06c8a090a850c90b
Anthony Morre <amorre92@gmail.com>
2019-12-10 14:38:13 +0100

    lecture 53

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('Test recipe', 'yummy test', 'https://www.google.be/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwje5IvulavmAhXoDmMBHZcMBVMQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.gimmesomeoven.com%2Fjambalaya-recipe%2F&psig=AOvVaw02p8qz5AUbH1EZFe_jzLlt&ust=1576070386248888')
+    new Recipe('Test recipe', 'yummy test', 'http://www.club-sandwich.net/images/photorecettes/burger-regime-123rf.jpg'),
+    new Recipe('Barbecue recipe', 'yummy test 2', 'https://favorflav.com/images/shutterstock_403751194-916x458.jpg')
   ];
 
   constructor() { }
